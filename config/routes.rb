191a3ffce4efdc56cc96b89250dce1f3f47b456f
Kristina Chang <kristinachang@users.noreply.github.com>
2016-07-12 02:08:41 -0700

    add filter to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,56 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'home#index'
+  get '/about', to: 'home#about', as: 'about'
+  get '/contact', to: 'home#contact', as: 'contact'
 
   resources :recipes, only: [:index, :show, :create, :update, :destroy]
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+=begin
+                  Prefix Verb   URI Pattern                    Controller#Action
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+                teaspoon        /teaspoon                      Teaspoon::Engine
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+        new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+            user_session POST   /users/sign_in(.:format)       devise/sessions#create
+    destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+           user_password POST   /users/password(.:format)      devise/passwords#create
+       new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+      edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+                         PATCH  /users/password(.:format)      devise/passwords#update
+                         PUT    /users/password(.:format)      devise/passwords#update
+cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+       user_registration POST   /users(.:format)               devise/registrations#create
+   new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+  edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+                         PATCH  /users(.:format)               devise/registrations#update
+                         PUT    /users(.:format)               devise/registrations#update
+                         DELETE /users(.:format)               devise/registrations#destroy
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+                    root GET    /                              home#index
+                   about GET    /about(.:format)               home#about
+                 contact GET    /contact(.:format)             home#contact
+                 
+                 recipes GET    /recipes(.:format)             recipes#index
+                         POST   /recipes(.:format)             recipes#create
+                  recipe GET    /recipes/:id(.:format)         recipes#show
+                         PATCH  /recipes/:id(.:format)         recipes#update
+                         PUT    /recipes/:id(.:format)         recipes#update
+                         DELETE /recipes/:id(.:format)         recipes#destroy
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+Routes for Teaspoon::Engine:
+   root GET  /                             teaspoon/suite#index
+fixture GET  /fixtures/*filename(.:format) teaspoon/suite#fixtures
+  suite GET  /:suite(.:format)             teaspoon/suite#show {:suite=>"default"}
+        POST /:suite/:hook(.:format)       teaspoon/suite#hook {:suite=>"default", :hook=>"default"}
+=end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
